c6bf7ac1033399c1e044046b8499b0ffadd0208f
Burak Tomakin <brk.tmkn@gmail.com>
2021-07-04 00:21:08 +0300

    detay sayfası güncellendi

--- Filmkolik/ClientApp/src/Films/FilmDetail.jsx
+++ Filmkolik/ClientApp/src/Films/FilmDetail.jsx
@@ -5,16 +5,33 @@ import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Color from "color";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   paperBgColor: {
     backgroundColor: Color("rgba(26, 25, 23, 0)").alpha(0.7).string(),
   },
   movieName: {
+    ...theme.typography.button,
     fontSize: "40px",
+    textAlign: "center",
+    color: Color("rgb(255, 255, 255)").string(),
   },
-  overview: {},
+  overview: {
+    color: Color("rgb(255, 255, 255)").string(),
+  },
+  tagLine: {
+    fontSize: "24px",
+    color: theme.palette.primary.light,
+  },
+  genre: {
+    color: theme.palette.primary.light,
+    fontSize: "24px",
+  },
+  prodComps: {
+    color: Color("rgb(255, 255, 255)").string(),
+  },
 }));
 
 function FilmDetail(props) {
@@ -52,14 +69,45 @@ function FilmDetail(props) {
     <div>
       <Container>
         <Paper className={classes.paperBgColor}>
-          <Box
-            component="img"
-            maxHeight="500px"
-            maxWidth="350px"
-            alt="The house from the offer."
-            src={tmdbInstancer.getLittleImage(film.poster_path)}
-          />
-          
+          <Grid container>
+            <Grid xs={3}>
+              <Box
+                component="img"
+                maxHeight="500px"
+                maxWidth="100%"
+                alt="The house from the offer."
+                src={tmdbInstancer.getLittleImage(film.poster_path)}
+              />
+            </Grid>
+            <Grid md={9}>
+              <Typography className={classes.movieName}>
+                {film.original_title}
+              </Typography>
+              <Box px={3}>
+                <Typography className={classes.tagLine}>
+                  {film.tagline}
+                </Typography>
+              </Box>
+              <Box px={3}>
+                <Typography className={classes.overview}>
+                  {film.overview}
+                </Typography>
+              </Box>
+              <Box p={3}>
+                <Typography className={classes.genre}>
+                  {film.genres && film.genres.map((genre) => genre.name).join(', ')}
+                </Typography>
+              </Box>
+              <Box px={3}>
+                <Typography className={classes.prodComps}>
+                  {film.production_companies &&
+                    film.production_companies
+                      .map((comp) => comp.name)
+                      .join(", ")}
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
         </Paper>
       </Container>
     </div>
